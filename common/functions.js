d9cfbff0eb185c359a4aef02f4844d23a8c11a7e
Gurjit Singh <gurjits1067@gmail.com>
2025-09-16 01:10:46 +0530

    Initialize Firebase only if credentials are available

--- common/functions.js
+++ common/functions.js
@@ -3,7 +3,8 @@ const Handlebars = require("handlebars");
 const axios = require('axios');
 const admin = require('firebase-admin');
 
-if (!admin.apps.length) {
+// Initialize Firebase only if credentials are available
+if (!admin.apps.length && process.env.FIREBASE_PRIVATE_KEY) {
   admin.initializeApp({
       credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
